218b86fd3971dadce6194534108b856fd19ea8df
Bartlomiej Ploszynski <bartlomiej.ploszynski@contractors.roche.com>
2019-04-29 15:39:31 +0200

    Code reformating.

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,39 @@
+const Event = require('../../models/event');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5cc6d6e130fe5231c4dbf6e2'
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById('5cc6d6e130fe5231c4dbf6e2');
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
