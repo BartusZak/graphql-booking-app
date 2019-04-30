c9c9972825728a3b0862d0582a397781a8bf18ca
Bartlomiej Ploszynski <bartlomiej.ploszynski@contractors.roche.com>
2019-04-30 08:52:45 +0200

    User authentication.

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
 const { transformEvent } = require('./merge');
 
 module.exports = {
@@ -12,19 +13,22 @@ module.exports = {
       throw err;
     }
   },
-  createEvent: async args => {
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: '5cc6d6e130fe5231c4dbf6e2'
+      creator: req.userId
     });
     let createdEvent;
     try {
       const result = await event.save();
       createdEvent = transformEvent(result);
-      const creator = await User.findById('5cc6d6e130fe5231c4dbf6e2');
+      const creator = await User.findById(req.userId);
       if (!creator) {
         throw new Error('User not found.');
       }
